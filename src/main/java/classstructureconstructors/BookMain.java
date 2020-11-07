3870b94804f59b548dd81580c9d5133a136ef7f7
BrigiBalogh <73614406+BrigiBalogh@users.noreply.github.com>
2020-11-07 22:10:58 +0100

    Constructors kész.

---
+++ src/main/java/classstructureconstructors/BookMain.java
@@ -0,0 +1,14 @@
+package classstructureconstructors;
+
+public class BookMain {
+
+    public static void main(String[] args) {
+        Book book= new Book ("Mijazaki Josifumi","Sinrin-joku");
+        
+        book.register("102030");
+
+        System.out.println("Szerző:"+book.getAuthor());
+        System.out.println("Könyv Címe:"+book.getTitle());
+        System.out.println("Regisztrációs szám:"+book.getRegNumber());
+    }
+}
