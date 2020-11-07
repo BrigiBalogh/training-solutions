b70258a0a1ac7af0dea460e03ba1b464ea525e15
BrigiBalogh <73614406+BrigiBalogh@users.noreply.github.com>
2020-11-07 11:04:53 +0100

    Song kész

---
+++ src/main/java/classstructureattributes/Music.java
@@ -0,0 +1,21 @@
+package classstructureattributes;
+
+import java.util.Scanner;
+
+public class Music {
+
+    public static void main(String[] args) {
+        song song= new song();
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Ki a kedvenc előadód ?");
+        song.band =scanner.nextLine();
+        System.out.println("Mi a kedvenc dalod címe?");
+        song.title =scanner.nextLine();
+        System.out.println("Hány perc hosszú?");
+        song.lenght=scanner. nextInt();
+
+        System.out.println(song.band+"-"+song.title+"("+song.lenght+"perc)");
+
+    }
+}
---
+++ src/main/java/classstructureattributes/song.java
@@ -0,0 +1,8 @@
+package classstructureattributes;
+
+public class song {
+
+    String band;
+    String title;
+    int lenght;
+}
