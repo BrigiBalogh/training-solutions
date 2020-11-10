91f4885ea61ccbf067e32a9456a8529a0f2ac380
BrigiBalogh <73614406+BrigiBalogh@users.noreply.github.com>
2020-11-10 23:48:06 +0100

    Qualifier is finished.

---
+++ src/main/java/introcontrol/introcontrol/Qualifier.java
@@ -0,0 +1,16 @@
+package introcontrol.introcontrol;
+
+import java.util.Scanner;
+
+public class Qualifier {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number <= 100) {
+            System.out.println("száz vagy kisebb");
+        } else {
+            System.out.println("nagyobb,mint száz");
+        }
+    }
+}
