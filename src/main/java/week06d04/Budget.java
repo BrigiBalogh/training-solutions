0821b581b9e7fcc0ea5b106b0a3935c9a313f25e
BrigiBalogh <73614406+BrigiBalogh@users.noreply.github.com>
2020-12-10 14:32:37 +0100

    Biscuit are finished.

--- src/main/java/week06d04/Budget.java
+++ src/main/java/week06d04/Budget.java
@@ -8,15 +8,19 @@ public class Budget {
     private List <Item> items;
 
     public Budget(List<Item> items) {
-        this.items = items;
+        this.items = new ArrayList<>(items);
     }
 
 
-    public List<Item> getItemsByMonth(int monthNumber) {
+    public List<Item> getItemsByMonth(int date) {
+        if (date < 1 || date > 12) {
+            throw new IllegalArgumentException( "Invalid date !");
+
+        }
         List<Item> m = new ArrayList<>();
 
         for (Item item : items) {
-            if (item.getMonth()==monthNumber) {
+            if (item.getMonth()== date) {
                 m.add(item);
             }
         }
