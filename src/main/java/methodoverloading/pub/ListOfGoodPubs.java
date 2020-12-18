d8d739aa0a6092818d3096a4233f897cc9d8b79b
BrigiBalogh <73614406+BrigiBalogh@users.noreply.github.com>
2020-12-18 10:13:10 +0100

    Pub and Time are finished.

---
+++ src/main/java/methodoverloading/pub/ListOfGoodPubs.java
@@ -0,0 +1,26 @@
+package methodoverloading.pub;
+
+import java.util.List;
+
+public class ListOfGoodPubs {
+
+    private List<Pub> goodPubs;
+
+    public ListOfGoodPubs(List<Pub> goodPubs) {
+        if(goodPubs.isEmpty() || goodPubs == null) {
+            throw new IllegalArgumentException("Pub list is empty !");
+        }
+        this.goodPubs = goodPubs;
+    }
+
+    public Pub findTheBest(){
+        Pub best = goodPubs.get(0);
+        for (int i= 1; i < goodPubs.size(); i++) {
+            if (goodPubs.get(i).getOpenFrom().isEarlier(best.getOpenFrom())) {
+                best = goodPubs.get(i);
+            }
+        }
+        return best;
+
+    }
+}
