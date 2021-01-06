75c5eb160764c660a4a6b8964b3c1bb3a76b3901
BrigiBalogh <73614406+BrigiBalogh@users.noreply.github.com>
2021-01-06 20:25:20 +0100

    Files are finished.

---
+++ src/main/java/week09d03/santaClaus.java
@@ -0,0 +1,17 @@
+package week09d03;
+
+import java.util.List;
+
+public class santaClaus {
+
+    private List<Person> persons;
+
+    public santaClaus(List<Person> persons) {
+        this.persons = persons;
+    }
+
+   public Present getThroughChimneys() {
+        for (Person person : persons)
+
+   }
+}
