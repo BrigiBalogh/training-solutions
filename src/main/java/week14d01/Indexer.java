6bf7310968b9c9638a1d850b6edbdc2afba8b251
BrigiBalogh <73614406+BrigiBalogh@users.noreply.github.com>
2021-02-10 07:14:07 +0100

    Week14d01 is finished.

---
+++ src/main/java/week14d01/Indexer.java
@@ -0,0 +1,23 @@
+package week14d01;
+
+import java.util.*;
+
+public class Indexer {
+
+    public Map<Character, List<String>> index(List<String> names) {
+        Map<Character,List<String>> result = new HashMap<>();
+        for (String name : names) {
+            char initial = name.charAt(0);
+            if (!result.containsKey(initial)) {
+                result.put(initial, new ArrayList<>());
+            }
+            result.get(initial).add(name);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Indexer().index(Arrays.asList("Odon", "Lujza", "Abraham", "Magdolna")));
+// {A=[Abraham], L=[Lujza], M=[Magdolna], O=[Odon]}
+    }
+}
