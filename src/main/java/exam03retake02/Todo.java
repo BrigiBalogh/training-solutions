3a1d365cfd0350f58e51edcf126e814cdde73929
BrigiBalogh <73614406+BrigiBalogh@users.noreply.github.com>
2021-02-26 19:01:41 +0100

    BalatonStorm

--- src/main/java/exam03retake02/Todo.java
+++ src/main/java/exam03retake02/Todo.java
@@ -4,19 +4,27 @@ package exam03retake02;
 
 public class Todo {
 
+    public static final int MAX_PRIORITY = 1;
+    public static final int MIN_PRIORITY = 5;
+
     private String text;
     private int priority;
     private State state;
 
     public Todo(String text, int priority) {
+        if (priority < MAX_PRIORITY || priority > MIN_PRIORITY) {
+            throw new IllegalArgumentException("priority invalid " + priority);
+        }
         this.text = text;
         this.priority = priority;
+        this.state = State.NON_COMPLETED;
     }
 
     public Todo(String text) {
     }
 
     public void complete() {
+        this.state = State.COMPLETED;
     }
 
 
--- src/main/java/exam03retake02/TodoList.java
+++ src/main/java/exam03retake02/TodoList.java
@@ -8,21 +8,40 @@ public class TodoList {
 
     private List<Todo> todos = new ArrayList<>();
 
-    public Object getNumberOfItemsLeft() {
-        return null;
+    public int getNumberOfItemsLeft() {
+        int count = 0;
+        for (Todo todo : todos) {
+            if (todo.getState().equals(State.NON_COMPLETED)) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public List<Todo> getTodos() {
         return todos;
     }
 
-    public Object getMostImportantTodosText() {
-        return null;
+    public List<String> getMostImportantTodosText() {
+        List<String> ret = new ArrayList<>();
+        int max = Todo.MIN_PRIORITY;
+        for (Todo todo : todos) {
+            if (todo.getPriority() < max) {
+                max = todo.getPriority();
+            }
+        }
+        for (Todo todo : todos) {
+            if (todo.getPriority() == max) {
+                ret.add(todo.getText());
+            }
+        }
+        return ret;
     }
 
     public void deleteCompleted() {
     }
 
-    public void addTodo(Todo bevásárlás) {
+    public void addTodo(Todo todo) {
+        todos.add(todo);
     }
 }
