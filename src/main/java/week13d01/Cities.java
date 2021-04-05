f2e1a298c8feb21cea8136a3251cd2ef2f5ac56f
BrigiBalogh <73614406+BrigiBalogh@users.noreply.github.com>
2021-04-05 22:21:59 +0200

    week13d01 cities senior.

---
+++ src/main/java/week13d01/Cities.java
@@ -0,0 +1,70 @@
+package week13d01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cities {
+
+    public static final String LINE_SEPARATOR = ";";
+    public static final int INDEX_OF_CITY = 1;
+    public static final int INDEX_OF_POSTCODE = 0;
+    public static final int INDEX_OF_PART = 2;
+
+
+    public City findFirstCity (BufferedReader reader) {
+        List<City> cities = citiesReader(reader);
+        return getFirstCity(cities);
+    }
+
+    private City getFirstCity(List<City> cities) {
+        if (cities.size() == 0) {
+            throw new IllegalArgumentException("Lista is empty !");
+        }
+        cities.sort(new NameComparator());
+        return cities.get(0);
+    }
+
+    private List<City> citiesReader(BufferedReader reader) {
+        List<City> cities = new ArrayList<>();
+        String line;
+        try {
+            skipHeader(reader);
+            while ((line = reader.readLine()) != null) {
+                City city = parseCity(line);
+                cities.add(city);
+            }
+        }
+        catch (IOException ioe) {
+            throw new IllegalStateException("Cannot read file",ioe);
+        }
+        return cities;
+    }
+
+    private City parseCity(String line) {
+        String[] parts = line.split(LINE_SEPARATOR);
+        if (parts.length > 2) {
+            String part= parts[INDEX_OF_PART];
+        }
+        City city = new City(parts[INDEX_OF_POSTCODE], parts[INDEX_OF_CITY], parts[INDEX_OF_PART]);
+        return city;
+    }
+
+    public void skipHeader(BufferedReader reader) throws IOException {
+        reader.readLine();
+    }
+
+    public static void main(String[] args) {
+        Cities cities = new Cities();
+        try(BufferedReader reader = Files.newBufferedReader(Path.of("irányitoszamok-varosok-2021.csv"))){
+           City first = cities.findFirstCity(reader);
+            System.out.println(first);
+
+        }catch (IOException ioe) {
+            throw new IllegalStateException("Cannot read file !",ioe);
+        }
+    }
+}
